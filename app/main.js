abf0d598abf430dfdb9929137790789e0a8b94b9
Lenient <qianliu@hp.com>
2016-06-03 20:22:43 +0800

    Add baseUrl for require js.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
+'use strict';
 require.config({
-  /*baseUrl: '/',*/
+  baseUrl: '/',
   paths: {
     'angular': '../node_modules/angular/angular',
     'angular-ui-router': '../node_modules/angular-ui-router/release/angular-ui-router'
